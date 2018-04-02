2d755719b49260407f80ed0fcacf1cde9e21fb60
Austin J. Alexander <austinjalexander@gmail.com>
2018-04-01 22:16:27 -0700

    Add serve pkg

---
+++ pkg/serve/serve.go
@@ -0,0 +1,48 @@
+package serve
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	timeout = time.Second * 20
+)
+
+// Serve creates a new routed server and runs it.
+func Serve(port uint16) {
+	r := mux.NewRouter()
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("0.0.0.0:%d", port),
+		Handler:      r,
+		IdleTimeout:  timeout,
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
+	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	srv.Shutdown(ctx)
+	log.Println("info: gracefully shutting down server...")
+	os.Exit(0)
+}
---
+++ video/crop/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/satori/go.uuid"
+)
+
+const (
+	i = "/Users/thebigapple/Desktop/social-promo.mp4"
+)
+
+func main() {
+	var (
+		i = flag.String("i", "", "Input filepath")
+		w = flag.Uint("w", 0, "Crop width")
+		h = flag.Uint("h", 0, "Crop height")
+		x = flag.Uint("x", 0, "Crop top-left-most x")
+		y = flag.Uint("y", 0, "Crop top-left-most y")
+	)
+	flag.Parse()
+
+	outFilepath, err := Crop(*i, *w, *h, *x, *y)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\n\t***New cropped video file at %s!***\n", outFilepath)
+}
+
+// Crop makes system calls to ffmpeg to crop a video.
+func Crop(inFilepath string, w, h, x, y uint) (string, error) {
+	cmd, args, err := setCmdArgs(inFilepath, w, h, x, y)
+	if err != nil {
+		return "", err
+	}
+
+	stdOutErr, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("error: %s: %s", stdOutErr, err)
+	}
+	return args[len(args)-1], nil
+}
+
+func outFilepath() (string, error) {
+	vidUUID, err := uuid.NewV4()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s.mp4", filepath.Join(os.TempDir(), vidUUID.String())), nil
+}
+
+func setCmdArgs(inFilepath string, w, h, x, y uint) (string, []string, error) {
+	out, err := outFilepath()
+	if err != nil {
+		return "", nil, err
+	}
+
+	cmd := "/usr/local/bin/ffmpeg"
+	args := []string{
+		"-i",
+		inFilepath,
+		"-vf",
+		fmt.Sprintf("crop=%d:%d:%d:%d", w, h, x, y),
+		"-c:a",
+		"copy",
+		out,
+	}
+
+	return cmd, args, nil
+}
