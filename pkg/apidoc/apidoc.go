9ee58bc7e06753029ebd1ee7eb05259c218e468e
Austin J. Alexander <austinjalexander@gmail.com>
2018-04-02 19:14:48 -0700

    Add flush to apidoc

--- pkg/apidoc/apidoc.go
+++ pkg/apidoc/apidoc.go
@@ -1,7 +1,11 @@
 package apidoc
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/austinjalexander/util/pkg/server"
 )
@@ -27,10 +31,38 @@ func (d Doc) Write(h server.Handler) server.Handler {
 }
 
 // Flush closes the handler channel and writes the documentation to disk.
-func (d Doc) Flush() {
-	fmt.Println("HEY")
+func (d Doc) Flush() error {
 	close(d.handlerChan)
+
 	for h := range d.handlerChan {
 		fmt.Println(h)
+
+		dir := filepath.Join(d.baseDir, h.Path)
+		err := os.MkdirAll(dir, 0666)
+		if err != nil {
+			return err
+		}
+
+		f, err := os.Create(filepath.Join(dir, filepath.Base(h.Path)+".md"))
+		if err != nil {
+			return err
+		}
+		defer func() {
+			err := f.Close()
+			if err != nil {
+				log.Println(err)
+			}
+		}()
+
+		w := bufio.NewWriter(f)
+
+		for _, method := range h.Methods {
+			_, err := w.WriteString(fmt.Sprintf("# %s `%s`\n\n", method, h.Path))
+			if err != nil {
+				return err
+			}
+		}
+		w.Flush()
 	}
+	return nil
 }
